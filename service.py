39c4ccff6640ffb8f10b664cbe698416bd1f3440
Jose Antonio <josew383@gmail.com>
2017-12-27 23:56:12 +0100

    Improve addon

--- service.py
+++ service.py
@@ -8,7 +8,6 @@ bulb1 = "192.168.4.230"
 bulb2 = "192.168.4.252"
 bulb3 = "192.168.4.85"
 bulb4 = "192.168.4.232"
-
 #-------------------------------------------------------------------------
 #Method of yeelight
 
@@ -32,11 +31,11 @@ def operate_on_bulb(ip, method, params):
 
 def set_rgb(ip, color):
     #white 16777215 blue 255 green 65280 red 16711680 pink 16711935 yellow 16776960 turquoise 65535
-    params=",\"sudden\",500"
+    params=",\"smooth\",500"
     operate_on_bulb(ip, "set_rgb", str(color)+params)
 
 def set_bright(ip, bright):
-    params=",\"sudden\",500"
+    params=",\"smooth\",500"
     operate_on_bulb(ip, "set_bright", str(bright)+params)
     #effect (str)  The type of effect. Can be "smooth" or "sudden".
     #Minimun of bright is 1!!
@@ -67,19 +66,23 @@ def turn_off_all():
 
 def setMovieScene():
     #turn_off_all()
-    #turn_on(bulb4)
-    set_rgb(bulb4,255)
+    turn_on(bulb4)
+    set_rgb(bulb4,9599999)
     set_bright(bulb4,50)
 
 def setDefaultScene():
-	set_rgb(bulb4,65280)
-	set_bright(bulb4,100)
+    set_rgb(bulb4,16777215)
+    set_bright(bulb4,100)
 
 #-------------------------------------------------------------------------
 #MAIN OF YEELIGHTPRO
+#List of conditions: http://kodi.wiki/view/List_of_boolean_conditions
+isPlaying = False
 while True:
-	if xbmc.getCondVisibility('Player.HasMedia'):
-		setMovieScene()
-	else:
-		setDefaultScene()
-	time.sleep(3)
+    if xbmc.getCondVisibility('Player.Playing'):
+        setMovieScene()
+        isPlaying=True
+    elif isPlaying == True:
+        setDefaultScene()
+        isPlaying=False
+    time.sleep(10)
